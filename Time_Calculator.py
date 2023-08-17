22cc2993d1e41ab41b20a07c455ab01f793e4beb
Noann Selegar <noannselegar@gmail.com>
2023-08-17 19:24:01 -0300

    working except day of the week

--- Time_Calculator.py
+++ Time_Calculator.py
@@ -6,38 +6,30 @@ def add_time(start_time, duration, day=None):
     start_time = start_time.replace(':', ' ').split()
     duration = duration.split(':')
 
-    def HowLongFor(st0, st1, st2):
-        if st2 == 'PM':
-            hourdif = 11 - int(st0)
-            if st1 != '00':
-                mindif = 60 - int(st1)
+    def HowLongFor(hrs, min, timezone):
+        if timezone == 'PM':
+            hourdif = 11 - int(hrs)
+            if min != '00':
+                mindif = 60 - int(min)
             else:
                 mindif = 0
                 hourdif += 1
-        if st2 == 'AM':
-            hourdif = 23 - int(st0)
-            if st1 != '00':
-                mindif = 60 - int(st1)
+        if timezone == 'AM':
+            hourdif = 23 - int(hrs)
+            if min != '00':
+                mindif = 60 - int(min)
             else:
                 mindif = 0
                 hourdif += 1
         minut = hourdif*60 + mindif
         return minut
-
-    def ToggleTZ(t, MinutesLeft, MinutesToComp):
-        n = 0
-        if MinutesToComp < MinutesLeft:
-            return t
-        elif MinutesToComp >= MinutesLeft:
-            RealMinutes = MinutesToComp - MinutesLeft
-            NumOfTog = RealMinutes//720
-            if NumOfTog > 1:
-                n += NumOfTog
-            for _ in range(n+1):
-                if t == 'AM': t = 'PM'
-                else: t = 'AM'
-            return t
-
+      
+    def Toggle(tt):
+        if tt == 'AM': 
+            tt = 'PM'
+        else: 
+            tt = 'AM'
+        return tt
 
     def ChangeDay(MinutesToComp, MinutesLeft):
         if MinutesToComp >= MinutesLeft:
@@ -49,22 +41,24 @@ def add_time(start_time, duration, day=None):
         else:
             return 'same day'
 
-    def Hour(Min, Smin):
+    def Hour(Min, Smin, t):
         M = Min+Smin
         h = (M-(M%60))//60
-        if h > 12:
+        if h >= 12:
             for x in range(h//12):
                 h -= 12
-        return h
+                t = Toggle(t)
+        if h == 0: 
+            h = 12
+        return h, t
     
     howlong = HowLongFor(start_time[0], start_time[1], start_time[2])
     startminutes = int(start_time[0])*60+int(start_time[1])
     minutes = int(duration[0])*60+int(duration[1])
-    stop_hour = Hour(minutes, startminutes)
+    stop_hour, tz = Hour(minutes, startminutes, start_time[2])
     whichday = ChangeDay(minutes, howlong)
-    tz = ToggleTZ(start_time[2], howlong, minutes)
 
-    stop_string = f'{stop_hour}:{minutes%60:02d} {tz} {whichday}'
+    stop_string = f'{stop_hour}:{(startminutes+minutes)%60:02d} {tz} {whichday}'
     print(stop_string)
-
+    print(howlong, minutes)
             
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from Time_Calculator import add_time
 
-add_time('3:00 PM', '33:00', 'Monday')
+add_time('3:01 AM', '35:00')
